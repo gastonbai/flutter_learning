f551c6fad8fc42a10dadcf5605ab401c981a472d
gastonbai <danilschewtzov@yandex.ru>
2024-07-22 03:10:01 +0300

    Изучен Asset, добавление картинок в проект

--- lib/main.dart
+++ lib/main.dart
@@ -3,93 +3,28 @@ import "dart:async";
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(first());
+  runApp(MyApp());
 }
 
-class first extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _firstState();
-  }
-}
-
-class _firstState extends State<first> {
-  //нижнее подчеркивание - приватность
-
-   bool _loading= false;
-   double _progressValue = 0.0;
-
-  void initState() {
-    _loading = false;
-    _progressValue = 0.0;
-    super.initState();
-  }
-
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.pinkAccent,
         appBar: AppBar(
-          backgroundColor: Colors.pink,
-          title: Text('MyFirstApp'),
-          centerTitle: true,
+          title: Text('Adding asset'),
         ),
         body: Center(
-          child: Container(
-            padding: EdgeInsets.all(16),
-            child: _loading?
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                LinearProgressIndicator(
-                  value: _progressValue,
-                ),
-                Text(
-                  '${(_progressValue * 100).round()}%',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-
-              ],
-            )
-                : Text(
-              'Press button to download',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-              ),
-            ),
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image(image: AssetImage('assets/images/12313123.jpg')),
+              Image.asset('assets/icons/Brjy.png')
+            ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _loading = !_loading;
-              _updateProgress();
-            });
-
-          },
-          child: Icon(Icons.access_alarm_outlined),
-        ),
-      ),
+      )
     );
   }
-  void _updateProgress() {
-    const sec = const Duration(seconds: 1);
-    Timer.periodic(sec, (Timer t) {
-      setState(() {
-        _progressValue += 0.2;
 
-        if (_progressValue.toStringAsFixed(1) == '1.0'){
-          _loading = false;
-          t.cancel();
-          _progressValue = 0.0;
-          return;
-        }
-      });
-    });
-  }
 }
