d96fa7f55d2b2ebdbff7c48e2102409ef1fbb3a6
gastonbai <danilschewtzov@yandex.ru>
2024-07-23 23:06:44 +0300

    Изучен Font, добавлен кастомный шрифт, изучено применение шрифтак как на отдельный текст, так и на весь виджет, также есть идеи сделать файл настроки для более быстрого добавления шрифта и для очистки кода(это для большого проекта)

--- lib/main.dart
+++ lib/main.dart
@@ -6,25 +6,35 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Adding asset'),
-        ),
-        body: Center(
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              Image(image: AssetImage('assets/images/12313123.jpg')),
-              Image.asset('assets/icons/Brjy.png')
-            ],
+      theme: ThemeData(fontFamily: 'PlaywriteHRLijeva'), //Весь виджет
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Adding asset'),
           ),
-        ),
-      )
-    );
+          body: Center(
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Image(image: AssetImage('assets/images/12313123.jpg')),
+                Image.asset('assets/icons/Brjy.png'),
+                Positioned(
+                    top: 16,
+                    left: 115,
+                    child: Text(
+                      'my castom font',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        // fontFamily: 'PlaywriteHRLijeva', //только конкретный текст
+                      ),
+                    )),
+              ],
+            ),
+          ),
+        ));
   }
-
 }
